6caf14ee0d165f84cef1fbc052f28d346c70c7dc
WangJunZzz <510423039@qq.com>
2019-10-16 11:15:48 +0800

    init

---
+++ Fairhr.Jobs/Startup.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hangfire;
+using Hangfire.Console;
+using Hangfire.HttpJob;
+using Hangfire.MySql.Core;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Fairhr.Jobs
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; set; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHangfire(ConfigurationHangfire);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseHangfireServer();
+            app.UseHangfireDashboard("/job");
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+
+        #region Hangfire配置
+        private void ConfigurationHangfire(IGlobalConfiguration globalConfiguration)
+        {
+            globalConfiguration.UseStorage(
+                new MySqlStorage(Configuration["jobdb"]))
+                .UseConsole()
+                .UseHangfireHttpJob(new HangfireHttpJobOptions()
+                {
+                    DashboardName = "泛亚统一任务调度平台"
+                });
+        }
+        #endregion
+    }
+}
